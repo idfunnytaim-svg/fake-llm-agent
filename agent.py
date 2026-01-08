2cbc155b8b181ba3d80ba95ac1ce4f5c720695b5
hongseonghyeon <idfunnytaim@gmail.com>
2026-01-08 15:35:23 +0900

    feat: agent 멘트수정

--- agent.py
+++ agent.py
@@ -5,6 +5,6 @@ class LlmAgent:
   def handle(self, user, message):
     if "weather" in message:
       weather = get_weather("seoul")
-      return f"{user} is '{weather}'"
+      return f"{user} is '{weather}입니다'"
     # 아주 단순한 LLM 흉내
     return f"{user}님, '{message}' 잘 받았습니다."
